7b3c5e1bf085c23777d22282761e82b8539c2c31
81rdy <101480715+81rdy@users.noreply.github.com>
2022-03-12 22:38:42 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#/bin/python3
+#import os ???
+import socket
+import colorama
+from colorama import Fore
+ip = input('insert IP/domain name: ')
+#"109.196.55.58"
+print('Hey, hey calm down! Its a lazy scanner he needs some time to do streaches and such to start working.\n\nGo make yourself cup of coffee or tea.\n')
+def longscan(ip):    
+    for port in range(6,1000):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = sock.connect_ex((ip,port))
+        if result == 0: 
+            print (Fore.RED+'!!!~Port '+str(port)+' is open~!!!')
+        else: 
+            print (Fore.WHITE+f'Port {port} is closed')
+        sock.close()
+longscan(ip)
