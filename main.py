ee06b73db7c18aaa3ce2290840cd2560596748b6
81rdy <101480715+81rdy@users.noreply.github.com>
2022-03-31 00:30:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #/bin/python3
-<<<<<<< HEAD
 #import os ???
 import socket
 from colorama import Fore
@@ -92,20 +91,3 @@ while True:
     choice(mode)
     print(Fore.RED+f'DONE!\n\n')
     
-=======
-import socket
-from colorama import Fore
-ip = input('insert IP/domain name: ')
-print('Hey, hey calm down! Its a lazy scanner he needs some time to do streaches and such to start working.\n\nGo make yourself cup of coffee or tea.\n')
-def longscan(ip):    
-    for port in range(6,1000):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        result = sock.connect_ex((ip,port))
-        if result == 0: 
-            print (Fore.RED+'!!!~Port '+str(port)+' is open~!!!')
-        else: 
-            print (Fore.WHITE+f'Port {port} is closed')
-        sock.close()
-longscan(ip)
->>>>>>> upstream/main
