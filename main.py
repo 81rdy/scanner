31c6d6443dac0f0244924b980cab4e08f5f34c33
81rdy <101480715+81rdy@users.noreply.github.com>
2022-07-12 21:40:19 +0200

    adding IP sweep for local network
    it doesn't work kinda
    you need first ping the ip and after that it will notice it 
    TODO
    repair the thing lol

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 #/bin/python3
 
+            # TODO add possiblity to make a list of ip addresses to scan.
 
-#import os ???
-
+import os
 import socket
 from colorama import Fore
+print('Hey, hey calm down! Its a lazy scanner it needs some time to do some streaches and such to start working.\n\nGo make yourself cup of coffee or tea.\n')
+local = input('do you want to scan local network for potentail targets?:\n[y/N]:')
+sweep = """
+#!/bin/bash
 
-#print('Hey, hey calm down! Its a lazy scanner he needs some time to do streaches and such to start working.\n\nGo make yourself cup of coffee or tea.\n')
+local_networks=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){2}[0-9]*' | grep -Eo '([0-9]*\.){2}[0-9]*' | grep -v '127.0.0')
+echo $local_networks
+for network in ${local_networks[@]}; do
+    for ip in `seq 1 254`; do 
+        ping -c 1 $network.$ip | grep "64 bytes" | cut -d " " -f 4 &
+    done
+done
 
+"""
+if local == "y":
+    os.system(sweep)
 
 def choice(mode):
     match mode:
@@ -25,15 +38,15 @@ def choice(mode):
 
 def fullscan(ip):
     print(Fore.GREEN+'->FULLSCAN!<-')    
-    for port in range(1,65535):
+    for port in range(2,65535):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = sock.connect_ex((ip,port))
         
         if result == 0:
             print (Fore.RED+f'!!!~Port {port} is open~!!!')
-        else: 
-            print (Fore.WHITE+f'Port {port} is closed')
+        #else: 
+            #print (Fore.WHITE+f'Port {port} is closed')
         sock.close()
 
 
@@ -51,7 +64,7 @@ def custom_range(ip):
                 if result == 0: 
                     print (Fore.RED+f'!!!~Port {port} is open~!!!')
                 #else: 
-                    #print (Fore.WHITE+f'Port {port} is closed')
+                    #print (Fore.WHITE+f'Port {port1} is closed')
                 sock.close()
         except ValueError:
             print(Fore.RED+"ENTER A PROPER VALUE >:(")
@@ -67,7 +80,7 @@ def most_popular(ip):
             socket.setdefaulttimeout(1)
             result = sock.connect_ex((ip,port1))
             if result == 0:
-                print (Fore.WHITE+f'!!!~Port {port1} is open~!!!')
+                print (Fore.RED+f'!!!~Port {port1} is open~!!!')
             #else:
                 #print (Fore.WHITE+f'Port {port1} is closed')
             sock.close()
@@ -84,7 +97,9 @@ def custom_list(ip):
             socket.setdefaulttimeout(1)
             result = sock.connect_ex((ip,port1))
             if result == 0:
-                print (Fore.WHITE+f'!!!~Port {port1} is open~!!!')
+                print (Fore.RED+f'!!!~Port {port1} is open~!!!')
+            #else:
+                #print (Fore.WHITE+f'Port {port1} is closed')
             sock.close()
 
 
@@ -93,3 +108,4 @@ while True:
     mode = input(Fore.YELLOW+'pick scanner mode 1-4\n\t>1.fullscan\n\t>2.custom range\n\t>3.most popular\n\t>4.custom list\n======>')
     choice(mode)
     print(Fore.RED+f'DONE!\n\n')
+    
